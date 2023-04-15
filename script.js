7bb6bcb0e7b7ee227bc99ec01ca7b564ec62ff0e
klhawker <kai.lanoe@gmail.com>
2023-04-15 18:22:11 +0200

    basically finished

--- script.js
+++ script.js
@@ -5,18 +5,52 @@ for (let i = 0; i < 16; i++) {
 }
 
 const container = document.querySelector(".container");
-const grid = document.querySelectorAll(".grid");
 
-grid.forEach((item) => {
-    item.addEventListener('mouseover', changeColour)
-});
+function attachEventListeners() {
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach((item) => {
+        item.addEventListener('mouseenter', changeColour)
+    });
+}
 
 function changeColour() {
     this.style.backgroundColor = 'blue';
 }
 
 function reset() {
-    grid.forEach((item) => {
+    document.querySelectorAll(".grid").forEach((item) => {
         item.style.backgroundColor = 'white';
     });
 }
+
+
+function generateGrid(number) {
+    for (let i = 0; i < number; i++) {
+        const grid_div = document.createElement("div");
+        grid_div.setAttribute('class', 'grid');
+        document.getElementsByClassName("container")[0].appendChild(grid_div);
+    }
+    return;
+}
+
+function changeGridSize() {
+    let input = prompt('Choose a grid size', '8');
+    if (input > 100) {
+        window.alert('Cant go above 100!')
+        return;
+    }else {
+        document.querySelectorAll(".grid").forEach((item) => {
+            item.remove();
+        });
+        generateGrid(input * input);
+        const grid_dimensions = `
+            width: ${1/input * 100}%;    
+            height: ${1/input * 100}%;    
+        `;
+        document.querySelectorAll(".grid").forEach((item) => {
+            item.style.cssText = grid_dimensions;
+        });
+        attachEventListeners();
+    }
+}
+
